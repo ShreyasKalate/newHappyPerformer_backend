b26f9e39178b17940afa6dbb83890cb862bc186a
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-05-03 11:58:35 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'HappyDB',
-       'USER': 'postgre',
+       'USER': 'postgres',
        'PASSWORD': 'undertaker',
        'HOST': 'localhost',
        'PORT': '5432',
